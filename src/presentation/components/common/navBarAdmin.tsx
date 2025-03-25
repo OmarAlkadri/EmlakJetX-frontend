5222f4fa179854ca54114000a1dc84a334402f4b
OmarAlkadri <omar.omar.alkadri111@gmail.com>
2025-03-25 03:30:57 +0300

    last update

--- src/presentation/components/common/navBarAdmin.tsx
+++ src/presentation/components/common/navBarAdmin.tsx
@@ -92,7 +92,7 @@ export const NavBarAdmin = () => {
                                         data-dropdown-toggle="dropdownNavbar"
                                         className="flex dark:text-black items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                                     >
-                                        Admin
+                                        Dashboard
                                         <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                                         </svg>
@@ -162,7 +162,7 @@ export const NavBarAdmin = () => {
                                     data-dropdown-toggle="dropdownNavbar"
                                     className="flex items-center justify-between w-full py-2 px-3 text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                                 >
-                                    Admin
+                                    Dashboard
                                     <svg className="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 4 4 4-4" />
                                     </svg>
